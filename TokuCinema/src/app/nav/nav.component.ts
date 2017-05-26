a40319ba6884d9c66cf9c241f00461894d391ab0
josephbayes <joseph.bayes@modea.com>
2017-05-26 00:01:38 -0400

    began work on nav component

--- TokuCinema/src/app/nav/nav.component.ts
+++ TokuCinema/src/app/nav/nav.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../css/app/nav/styles/styles.css']
 })
 export class NavComponent implements OnInit {
+  navMenuOpen: boolean = false;
+  searchOpen: boolean = false;
+  menuItems: Array<{"text": string, "link": string}> = [
+    {"text": 'Home', "link": '/home'},
+    {"text": 'About', "link": '/about'},
+    {"text": 'Movies', "link": '/movies'},
+    {"text": 'Media', "link": '/media'}
+  ];
+  searchTerm: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private toggleNavMenu(): void {
+    this.navMenuOpen = !this.navMenuOpen;
+  }
+
+  private toggleSearch(): void {
+    this.searchOpen = !this.searchOpen;
   }
 
 }
