6c3d4ee98f45383dcb3f6106db881242c58dcbfe
josephbayes <joseph.bayes@modea.com>
2017-05-27 01:52:14 -0400

    more work on movies list

--- TokuCinema/src/app/movies/movies.component.ts
+++ TokuCinema/src/app/movies/movies.component.ts
@@ -7,13 +7,13 @@ import { ListMovieItem } from '../../domain/ListMovieItem';
 })
 export class MoviesComponent implements OnInit {
   movies: Array<ListMovieItem> = [
+    new ListMovieItem("Godzilla", "Gojira", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRT2ObE6c9lJr2ACGmOuPKpX2W5JOTalB59dAdBOU1_ThrLcx9", 1954, "Toho", "http://placehold.it/50x50"),
     new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
     new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
+    new ListMovieItem("Godzilla", "Gojira", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRT2ObE6c9lJr2ACGmOuPKpX2W5JOTalB59dAdBOU1_ThrLcx9", 1954, "Toho", "http://placehold.it/50x50"),
     new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
     new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
-    new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
-    new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50"),
-    new ListMovieItem("Godzilla", "Gojira", "http://placehold.it/200x200", 1954, "Toho", "http://placehold.it/50x50")
+    new ListMovieItem("Godzilla", "Gojira", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRT2ObE6c9lJr2ACGmOuPKpX2W5JOTalB59dAdBOU1_ThrLcx9", 1954, "Toho", "http://placehold.it/50x50")
   ];
 
   constructor() { }
