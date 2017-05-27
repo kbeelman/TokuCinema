a36eb0513e7638e65f4e3513ce55bb16718df864
josephbayes <joseph.bayes@modea.com>
2017-05-26 23:39:42 -0400

    cleaned styles

--- TokuCinema/src/app/dev-card/dev-card.component.ts
+++ TokuCinema/src/app/dev-card/dev-card.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dev-card',
-  templateUrl: './dev-card.component.html',
-  styleUrls: ['./dev-card.component.css']
+  templateUrl: './dev-card.component.html'
 })
 export class DevCardComponent implements OnInit {
   @Input() developer: {"name": string, "imgUrl": string, "bio": string};
