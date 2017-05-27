0cdcbd4e296c079d2b2cd25d4eadcde6815b2e7e
Joseph Bayes <Joseph Bayes>
2017-05-27 14:09:38 -0400

    More Updates to Media Detail

--- TokuCinema/src/app/mediadetails/mediadetails.component.ts
+++ TokuCinema/src/app/mediadetails/mediadetails.component.ts
@@ -7,9 +7,12 @@ import { MediaDetails } from '../../domain/MediaDetails';
 })
 export class MediadetailsComponent implements OnInit {
     movie: MediaDetails = new MediaDetails("Godzilla",
-    ["Aspect Ratio: 4x3", "Runtime: 90 minutes"],
-    ["Region: 1", "Number of Discs: 2"],
-    ["Distributor: Classic Media", "Catalog Code: 1234"]);
+    "ゴジラ (Gojira)",
+    ["Aspect Ratio: 4x3", "Runtime: 90 minutes", "Black & White", "Runtime: 96 (Japanese) / 80 (American)",
+        "Original Runtime: 96", "Chapter Stops: 12 (Japanese) / 5 (American)", "Subtitles: English"],
+    ["Medium: DVD", "Format: Single Sided, Dual-Layer", "Region: 1", "Number of Discs: 2", "Color System: NTSC"],
+    ["Distributor: Classic Media", "Catalog Code: 1234", "UPC: 0123456789", "Release Date: 2006"],
+    []);
 
   constructor() { }
 
--- TokuCinema/src/domain/MediaDetails.ts
+++ TokuCinema/src/domain/MediaDetails.ts
@@ -1,8 +1,10 @@
 export class MediaDetails {
     constructor(
         public Title: string,
+        public TitleJapanese: string,
         public MainFeatureInfo: Array<string>,
         public DiscInfo: Array<string>,
-        public DistributionInfo: Array<string>
+        public DistributionInfo: Array<string>,
+        public PurchaseLinks: Array<string>
     ) {}
 }
