11e9855f4da6eb630b8e283ad8781e82cbf3ebae
josephbayes <joseph.bayes@modea.com>
2017-05-26 21:23:07 -0400

    changed homepage styling to include paragraphs

--- TokuCinema/src/app/home/home.component.ts
+++ TokuCinema/src/app/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../css/app/home/styles/styles.css']
 })
 export class HomeComponent implements OnInit {
-  landingPages: Array<{"text": string, "link": string}> = [
-    {"text": 'Movies', "link": '/movies'},
-    {"text": 'Media', "link": '/media'},
-    {"text": 'About', "link": '/about'}
+  landingPages: Array<{"text": string, "link": string, "body": string}> = [
+    {"text": 'Movies', "link": '/movies', 
+      "body": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
+    {"text": 'Media', "link": '/media', 
+      "body": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
+    {"text": 'About', "link": '/about', 
+      "body": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..'}
   ]
 
   constructor() { }
