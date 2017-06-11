243636adfb89d67086c9dd1ebe47038ddfff0eaa
Joey Bayes <bayesusmc343@gmail.com>
2017-06-10 22:46:24 -0400

    build 6-10-17 (2)

--- TokuCinema/src/app/about/about.component.ts
+++ TokuCinema/src/app/about/about.component.ts
@@ -8,13 +8,13 @@ export class AboutComponent implements OnInit {
   developers: Array<{"name": string, "imgUrl": string, "bio": string, "url": string}> = [
     {
       name: "Kiefer",
-      imgUrl: "http://placehold.it/250x150",
+      imgUrl: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/071/233/0cdb71d.jpg",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://www.linkedin.com/in/kbeelman/"
     },
     {
       name: "Joey",
-      imgUrl: "http://placehold.it/250x150",
+      imgUrl: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAX9AAAAJDQwYmRlODdjLTc2MTEtNDNmNS04OGEyLTJhZjQxNGM3MjJmMg.jpg",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://www.linkedin.com/in/josephwbayes/"
     }
