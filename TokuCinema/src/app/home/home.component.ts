e52ecc08a72c262f292bdbe4adb1db7e1eceaaf3
kbeelman <kiefer.beelman@gmail.com>
2017-06-14 01:35:42 -0400

    More home page text updates.

--- TokuCinema/src/app/home/home.component.ts
+++ TokuCinema/src/app/home/home.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   landingPages: Array<{"text": string, "link": string, "body": string}> = [
     {"text": 'Movie List', "link": '/movies', 
-      "body": 'Information all your favorite Tokusatsu, Giant Monster, and Sci-Fi films; including Godzilla, Gamera, and others.'},
+      "body": 'Information on all of your favorite Tokusatsu, Giant Monster, and Sci-Fi films; including Godzilla, King Kong, and more.'},
     {"text": 'Home Media Releases', "link": '/media', 
-      "body": 'Information across wide range of home media formats as well as reviews capturing everything from video and audio quality to special features.'},
+      "body": 'Details on home media releases across a wide range of formats, as well as reviews capturing everything from video quality to special features.'},
     {"text": 'About', "link": '/about', 
       "body": 'Background on the goals of this site and information about its developers.'}
   ]
