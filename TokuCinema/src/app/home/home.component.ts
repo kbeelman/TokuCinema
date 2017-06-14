c9ed1639c01dec2f98efa4a6002618f8f1593407
kbeelman <kiefer.beelman@gmail.com>
2017-06-13 22:09:09 -0400

    Changes the name of the movie and media sections, and updates the descriptions of each.

--- TokuCinema/src/app/home/home.component.ts
+++ TokuCinema/src/app/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   landingPages: Array<{"text": string, "link": string, "body": string}> = [
-    {"text": 'Media', "link": '/media', 
-      "body": 'Home media reviews capturing everything from video and audio quality to format and releasing studio.'},
-    {"text": 'Movies', "link": '/movies', 
-      "body": 'Reviews as well as objective data on all your favorite Tokusatsu films, including Godzilla, Mothra, and others.'},
+    {"text": 'Movie List', "link": '/movies', 
+      "body": 'Information all your favorite Tokusatsu, Giant Monster, and Sci-Fi films; including Godzilla, Gamera, and others.'},
+    {"text": 'Home Media Releases', "link": '/media', 
+      "body": 'Information across wide range of home media formats as well as reviews capturing everything from video and audio quality to special features.'},
     {"text": 'About', "link": '/about', 
       "body": 'Background on the goals of this site and information about its developers.'}
   ]
