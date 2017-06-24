e9c2c1924991867c7adf1af929886a2c23e3b62d
Joey Bayes <bayesusmc343@gmail.com>
2017-06-23 22:05:04 -0400

    fix test

--- TokuCinema/src/app/not-found/not-found.component.spec.ts
+++ TokuCinema/src/app/not-found/not-found.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule }   from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 import { NotFoundComponent } from './not-found.component';
 
@@ -8,7 +10,11 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
+      declarations: [ NotFoundComponent ],
+      imports: [ 
+        FormsModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
