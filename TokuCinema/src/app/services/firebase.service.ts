e9b8e7ecd72055a04bf6d3bb1079d60af5009388
josephbayes <joseph.bayes@modea.com>
2017-06-28 00:18:57 -0400

    started building item lookup in firebase service

--- TokuCinema/src/app/services/firebase.service.ts
+++ TokuCinema/src/app/services/firebase.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, Inject } from '@angular/core';
-import { Http, Response } from "@angular/http";
 import { Observable } from "rxjs/Rx";
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { StringCleaner, StringType } from '../../domain/StringCleaner';
+import { DomainBuilder, DataType } from '../../domain/Builder';
+import { Media } from '../../domain/Media';
+import { Movie } from '../../domain/Movie';
 
 @Injectable()
 export class FirebaseService {
-    constructor(private db: AngularFireDatabase) {}
+    constructor(
+      private db: AngularFireDatabase
+    ) {}
 
     getBranch(branchName: string): FirebaseListObservable<any> {
       let item: FirebaseListObservable<any>;
@@ -16,12 +20,24 @@ export class FirebaseService {
       return item;
     }
 
-    getMovieDetails(route: string): FirebaseListObservable<any> {
-      let item: FirebaseListObservable<any>;
+    getItemFromBranch(item: string, branchName: string, itemIsRoute: boolean, buildType: DataType): Observable<any> {
+      let response: FirebaseListObservable<any>;
+      let itemString = itemIsRoute? this.getPathFromRoute(item) : item;
 
-      item = this.db.list('/media');
+      return this.db.list('/' + branchName,
+        {
+          query: {
+            orderByChild: 'Path',
+            equalTo: itemString
+          }
+        }).map((res: Response) => this.extractDomainObject(res, buildType))
+        .catch(this.handleError);
+    }
 
-      return item;
+    extractDomainObject(res: Response, buildType: DataType): any {
+      let domainBuilder = new DomainBuilder(res[0], buildType);
+      let domainObject = domainBuilder.getDomainObject();
+      return domainObject;
     }
 
     getPathFromRoute(route: string): string {
