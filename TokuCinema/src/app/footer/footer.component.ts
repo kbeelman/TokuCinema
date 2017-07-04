91b5e711311a068b0075235bc0136e5f41346ee7
josephbayes <joseph.bayes@modea.com>
2017-07-04 00:09:13 -0400

    basic footer

--- TokuCinema/src/app/footer/footer.component.ts
+++ TokuCinema/src/app/footer/footer.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
-
+  year = new Date().getFullYear();
+  footerMessage: string = "&copy;TokuCinema " + this.year;
 
   constructor() { }
 
