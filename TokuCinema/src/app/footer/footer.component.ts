dafd78c8e7a1dccc4e9eab09bfc3906329040f8c
kbeelman <kiefer.beelman@gmail.com>
2017-07-05 18:48:15 -0400

    Adds a link in the Footer to the About page. Reduces the footer font-size.

--- TokuCinema/src/app/footer/footer.component.ts
+++ TokuCinema/src/app/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   year = new Date().getFullYear();
   footerMessage: Array<string> = [];
+  aboutLink: string = "/about";
 
   constructor() { }
 
