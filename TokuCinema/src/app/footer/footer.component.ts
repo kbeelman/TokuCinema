686b458c297e09e5e548cd98187785d8fe3ecdaf
kbeelman <kiefer.beelman@gmail.com>
2017-07-05 01:03:50 -0400

    Updates content of the footer.

--- TokuCinema/src/app/footer/footer.component.ts
+++ TokuCinema/src/app/footer/footer.component.ts
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   year = new Date().getFullYear();
-  footerMessage: string = "&copy;TokuCinema " + this.year;
+  footerMessage: Array<string> = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.setFooterMessage();
+  }
+
+  private setFooterMessage(): void {
+    this.footerMessage.push("All trademarks are the property of the respective trademark owners.");
+    this.footerMessage.push("© TokuCinema " + this.year);
   }
 
 }
