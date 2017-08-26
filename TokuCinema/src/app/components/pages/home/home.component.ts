54377ce02bfa42cfba089d08a8b32cb31541285a
Joey Bayes <bayesusmc343@gmail.com>
2017-08-26 00:41:22 -0400

    implemented first iteration of random content tile on homepage

--- TokuCinema/src/app/components/pages/home/home.component.ts
+++ TokuCinema/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,9 @@
+import { DataType } from '../../../domain/Builder';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { FirebaseService } from './../../../services/firebase.service';
+import { Media } from '../../../domain/Media';
 import { Component, OnInit } from '@angular/core';
+import { DomainBuilder } from "../../../domain/Builder";
 
 @Component({
   selector: 'app-home',
@@ -6,18 +11,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-  landingPages: Array<{"text": string, "link": string, "body": string}> = [
+  public randomContent: Media;
+  public landingPages: Array<{"text": string, "link": string, "body": string}> = [
     {"text": 'Movie List', "link": '/movies', 
       "body": 'Information on all of your favorite Tokusatsu, Giant Monster, and Sci-Fi films; including Godzilla, King Kong, and more.'},
     {"text": 'Home Media Releases', "link": '/media', 
-      "body": 'Details on home media releases across a wide range of formats, as well as reviews capturing everything from video quality to special features.'},
-    {"text": 'About', "link": '/about', 
-      "body": 'Background on the goals of this site and information about its developers.'}
+      "body": 'Details on home media releases across a wide range of formats, as well as reviews capturing everything from video quality to special features.'}
   ]
 
-  constructor() { }
+  constructor(private fdb: FirebaseService) {}
 
   ngOnInit() {
+    this.fdb.getBranch("media").subscribe((data) => {
+      
+      // Select a random index in the media branch to display in the content tile
+      let winningNumber = Math.floor(Math.random() * data.length) + 0;
+      
+      let builder: DomainBuilder = new DomainBuilder(data[winningNumber], DataType.Media);
+      this.randomContent = builder.getDomainObject();
+      console.log(this.randomContent);
+    });
   }
 
 }
