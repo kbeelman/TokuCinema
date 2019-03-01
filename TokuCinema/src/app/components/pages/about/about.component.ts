65d979754f0d6befde11aaf408eba1e59a214f83
Kiefer Beelman <kiefer.beelman@gmail.com>
2019-02-28 22:05:33 -0500

    Fixed pictures on About page.

--- TokuCinema/src/app/components/pages/about/about.component.ts
+++ TokuCinema/src/app/components/pages/about/about.component.ts
@@ -8,13 +8,13 @@ export class AboutComponent implements OnInit {
   developers: Array<{"name": string, "imgUrl": string, "bio": string, "url": string}> = [
     {
       name: "Kiefer",
-      imgUrl: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/071/233/0cdb71d.jpg",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/tokucinema.appspot.com/o/images%2Fabout%2Fkiefer.png?alt=media&token=c35652cf-4eab-4597-8320-d1b6a0fd792f",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://www.linkedin.com/in/kbeelman/"
     },
     {
       name: "Joey",
-      imgUrl: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAX9AAAAJDQwYmRlODdjLTc2MTEtNDNmNS04OGEyLTJhZjQxNGM3MjJmMg.jpg",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/tokucinema.appspot.com/o/images%2Fabout%2Fjoseph.png?alt=media&token=2054f594-d96c-4573-a285-abe24ec4caee",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://www.linkedin.com/in/josephwbayes/"
     }
