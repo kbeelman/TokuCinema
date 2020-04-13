afde6dacafdffedf0feeba8037d453975c6ba080
Kiefer Beelman <kiefer.beelman@gmail.com>
2020-04-12 20:37:11 -0400

    Added video support for alt countries. Stubbed some image support as well.

--- TokuCinema/src/app/domain/Country.ts
+++ TokuCinema/src/app/domain/Country.ts
@@ -2,6 +2,8 @@ import { Version } from './Version';
 
 export class Country {
     'Country': string;
+    'Videos': Array<{'Host': string, 'ID': string, 'Description': string}>;
+    'ScreencapDescriptions': Array<string>;
     'Active': boolean;
     'Versions': Array<Version>
 }
